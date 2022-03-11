ff09c4b1a3a11d27cd4a2167c07af50b753df244
Nuray-1-Dn <100020462+Nuray-1-Dn@users.noreply.github.com>
2022-03-11 14:49:17 +0600

    Add files via upload
    1 задание 5 неделя

---
+++ ппп/js/script.js
@@ -0,0 +1,31 @@
+"use script";
+
+//задание 1
+
+let tC=0;
+let tF=0;
+
+tC=Number(prompt("Введите темпертуру в градусах по Цельсию"));
+tF= (9/5)*tC+32;
+alert("Температура в грудусах по ффоренгейту:"+ String(tF));
+
+//Задание 2
+let admin;
+let userName;
+
+userName= "Нурай";
+admin=userName;
+console.log(admin);
+
+//Задание 3
+
+let result=10+10+"10"
+comsole.log(result);
+result =10+"10"+10;
+console.log(result);
+result =10+10+"10";
+console.log(result);
+result=10/-"";
+console.log(result);
+result=10 / + "2.5";
+console.log(result);
